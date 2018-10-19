b6f4f428ff76e3f986dce6c745494e26fd0647fb
ipkathon06 <43269120+ipkathon06@users.noreply.github.com>
2018-10-19 13:47:43 +0200

    Add files via upload
    v1.0 Start

---
+++ IPIKATHON/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Bernard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <div class="topBar">
+            <a href="..." class="topBarLangue"> Choisir la langue </a>
+            <a href=""><img class="topBarSpeak" src="image/micro.png"/></a>
+        </div>
+        <div>
+            <div><img class="imgAvatar" src="image/man.png"/></div>
+            <div><img class="imgDoubleArrow" src="image/double_arrow.png"/></div>
+        </div>
+    </body>
+
+</html>
